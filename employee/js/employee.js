f22df609c8025f2204c1f6099dd6e0f4e086c4dc
VeerbhadraPathakRewa <160132868+VeerbhadraPathakRewa@users.noreply.github.com>
2026-01-12 16:45:45 +0530

    lacture 4

--- employee/js/employee.js
+++ employee/js/employee.js
@@ -1,36 +1,11 @@
 window.onload = () => {
     collapsFunc();
     DynamicRequestFunc();
-setTimeout (()=>{
-    togglerFunc();
-    
-},100);
-}
-// toggler 
-//   const togglerFunc = () => {
-//             let togglerBtn = document.querySelector(".toggler-btn");
-//             let sideNav = document.querySelector(".side-nav");
-//             let page = document.querySelector(".page");
-//             console.log(togglerBtn);
-//             togglerBtn.onclick = () => {
-//         //    alert()
-//                 let open = sideNav.classList.contains("side-nav-open");
-//                 if (open == true) {
-//                     sideNav.classList.remove("side-nav-open");
-//                     sideNav.classList.add("side-nav-close");
-//                     page.classList.remove("page-open");
-//                     page.classList.add("page-close");
-//                 }
-//                 else {
-//                     sideNav.classList.add("side-nav-open");
-//                     sideNav.classList.remove("side-nav-close");
-//                     page.classList.add("page-open");
-//                     page.classList.remove("page-close");
-//                 }
-//             }
-//         }
-        
+    setTimeout(() => {
+        togglerFunc();
 
+    }, 100);
+}
 
 
 //Start Collaps Codding
@@ -51,20 +26,20 @@ const DynamicRequestFunc = () => {
     for (let el of allCollapsBtn) {
         el.onclick = (e) => {
             for (let el of allCollapsBtn) {
-                el.classList.remove("active")
+                el.classList.remove("active");
             }
             let link = e.target.getAttribute("access-link");
             dynamicAjaxFunc(link);
-            el.classList.add("active")
+            el.classList.add("active");
         }
     }
 }
 
 const dynamicAjaxFunc = (link) => {
-    let page = document.querySelector(".page")
+    let page = document.querySelector(".page");
     let ajax = new XMLHttpRequest();
     ajax.open("GET", link, true);
-    ajax.send()
+    ajax.send();
 
     // get resopnse
     ajax.onload = () => {
@@ -73,17 +48,27 @@ const dynamicAjaxFunc = (link) => {
         if (link == "dynamic/cat_designe.html") {
             createCategoryFunc();
         }
+        if (link == "dynamic/brand_designe.html") {
+            crateBrandFunc();
+        }
+
     }
 }
-//Start Create Category
+//Start Create Category coding
 const createCategoryFunc = () => {
+    let categoryList = document.querySelector(".category-list");
+    let categoryForm = document.querySelector(".category-form");
     let inputBoxEl = document.querySelector(".input-box");
     let addFieldBtn = document.querySelector(".add-field-btn");
+    let allcategoryData = [];
+    if (localStorage.getItem("allcategoryData") !== null) {
+        allcategoryData = JSON.parse(localStorage.getItem("allcategoryData"));
+    }
+    //start add field
     addFieldBtn.onclick = () => {
-
         inputBoxEl.innerHTML += ` <div>
                     <i style="curosr:pointer" class=" fa fa-trash mb-2 del-btn float-end"></i>
-                    <input class="form-control mb-3" placeholder="Category">
+                    <input class="form-control mb-3" required placeholder="Category">
                 </div>`;
         let allDelBtn = inputBoxEl.querySelectorAll(".del-btn");
         for (let btn of allDelBtn) {
@@ -92,4 +77,76 @@ const createCategoryFunc = () => {
             }
         }
     }
+    //start add category codding
+    categoryForm.onsubmit = (e) => {
+        e.preventDefault();
+        let allinput = categoryForm.querySelectorAll("input");
+        for (let input of allinput) {
+            allcategoryData.push({
+                category: input.value
+            });
+        }
+        localStorage.setItem("allcategoryData", JSON.stringify(allcategoryData));
+        swal("Success", "Your Category creation is Done!", "success");
+        readcategorydata();
+        categoryForm.reset('');
+
+    }
+    //Create category data
+    const readcategorydata = () => {
+        categoryList.innerHTML = "";
+        allcategoryData.forEach((data, index) => {
+            categoryList.innerHTML += `<tr index="${index}">
+                    <td>${index + 1}</td>
+                    <td>${data.category}</td>
+                    <td>
+                        <button class="btn btn-primary px-2 edit-btn "><i class=" fa fa-edit">Edit</i></button>
+                        <button class=" d-none btn btn-primary px-2 save-btn "><i class=" fa fa-save ">save</i></button>
+                        <button class="btn btn-danger px-2 del-btn "><i class=" fa fa-trash ">Delete</i></button>
+                    </td>
+                </tr>`;
+        });
+
+        //Category Delete Codding
+        let allDelBtn = categoryList.querySelectorAll(".del-btn");
+        for (let btn of allDelBtn) {
+            btn.onclick = () => {
+                let parent = btn.parentElement.parentElement;
+                let index = parent.getAttribute("index");
+                allcategoryData.splice(index, 1);
+                localStorage.setItem("allcategoryData", JSON.stringify(allcategoryData));
+                readcategorydata();
+            }
+        }
+        //Category Edit Codding
+        let allEditBtn = categoryList.querySelectorAll(".edit-btn");
+        for (let btn of allEditBtn) {
+            btn.onclick = () => {
+                let parent = btn.parentElement.parentElement;
+                let index = parent.getAttribute("index");
+                let allTD = parent.querySelectorAll("td");
+                let saveBtn = parent.querySelector(".save-btn");
+
+                allTD[1].contentEditable = true;
+                allTD[1].focus();
+                btn.classList.add("d-none");
+                saveBtn.classList.remove("d-none");
+                //Save Button Coding
+                saveBtn.onclick = () => {
+                    let category = allTD[1].innerHTML
+                    allcategoryData[index] = {
+                        category: category
+                    }
+                    localStorage.setItem("allcategoryData", JSON.stringify(allcategoryData));
+                    readcategorydata();
+                    allTD[1].contentEditable = false;
+                }
+            }
+        }
+    }
+    readcategorydata();
+}
+// Start Create Brand coding 
+const crateBrandFunc = () => {
+    alert();
 }
